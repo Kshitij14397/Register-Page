f35b364d7128896e9a9e44ab4f8b99b6cc7f48c3
Kshitij Pandey <kshitij14397@gmail.com>
2022-08-05 20:02:45 +0530

    added images folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import MyNav from './components/MyNav';
 import RegisterPage from './pages/register';
+import LoginPage from './pages/login';
 
 function App() {
   return (
     <>
     <MyNav/>
-    <RegisterPage/>
+    <LoginPage/>
     </>
   );
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import '../../index.css'
+import '../index.css'
+import './register.css'
 function LoginPage() {
     return (
 
